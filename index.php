f3487b08224550cee9937958b85eb721d43a085b
Soraia <soraialipe@gmail.com>
2025-02-06 17:12:51 -0300

    alterações no index

--- index.php
+++ index.php
@@ -12,6 +12,12 @@
     <title>Chuleta Quente Churrascaria</title>
 </head>
 <body class="fundofixo">
-    
+    <!-- Area de menu -->
+        <?php include 'menu_publico.php'?>
+        <a name="home">&nbsp;</a>
+        <main class="container">
+        <!-- area de carousel -->
+         <?php include 'carousel.php'?>
+        </main>
 </body>
 </html>
